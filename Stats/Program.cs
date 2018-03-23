378ad0af15cf8fadab39277ae54cb6aa503c88d8
Eric <wunderkammer123@gmail.com>
2018-03-23 08:57:32 -0400

    Added code to read from specific file in cwd. Using as a save point, keeping some 'notes' as comments for this commit.

--- Stats/Program.cs
+++ Stats/Program.cs
@@ -17,13 +17,41 @@ namespace Stats
 
             var files = directory.GetFiles("*.txt");
 
-            foreach (var file in files)
-            {
-                Console.WriteLine(file.Name);
-            }
+            // print all filename's in current working directory
+            // foreach (var file in files)
+            //{
+            //    Console.WriteLine(file.Name);
+            //}
 
-            // keeps console open until enter is pressed
-            Console.ReadLine();
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+
+            var file = new FileInfo(fileName);
+
+            // try-finally 
+            //if (file.Exists)
+            //{
+            //    var reader = new StreamReader(file.FullName);
+            //    try
+            //    {
+            //        Console.SetIn(reader);
+            //        Console.WriteLine(Console.ReadLine());
+            //    }
+            //    finally
+            //    {
+            //        reader.Close();
+            //    }
+            //}
+
+            if (file.Exists)
+            {
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());
+                }
+            }
+            // temporarily used to keep console open until user interaction
+            Console.ReadKey();
         }
     }
 }
