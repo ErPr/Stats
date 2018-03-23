93bf31cd901bdaf227fd6c319a09ebd9ac504f68
Eric <wunderkammer123@gmail.com>
2018-03-23 09:01:28 -0400

    Cleaned up commennts and formatting.

--- Stats/Program.cs
+++ Stats/Program.cs
@@ -11,36 +11,12 @@ namespace Stats
     {
         static void Main(string[] args)
         {
-            // GetCurrentDirectory() - gets the current working directory of the appplication
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
             var files = directory.GetFiles("*.txt");
-
-            // print all filename's in current working directory
-            // foreach (var file in files)
-            //{
-            //    Console.WriteLine(file.Name);
-            //}
-
             var fileName = Path.Combine(directory.FullName, "data.txt");
-
             var file = new FileInfo(fileName);
-
-            // try-finally 
-            //if (file.Exists)
-            //{
-            //    var reader = new StreamReader(file.FullName);
-            //    try
-            //    {
-            //        Console.SetIn(reader);
-            //        Console.WriteLine(Console.ReadLine());
-            //    }
-            //    finally
-            //    {
-            //        reader.Close();
-            //    }
-            //}
 
             if (file.Exists)
             {
@@ -50,7 +26,7 @@ namespace Stats
                     Console.WriteLine(Console.ReadLine());
                 }
             }
-            // temporarily used to keep console open until user interaction
+
             Console.ReadKey();
         }
     }
