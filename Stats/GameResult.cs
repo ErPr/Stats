a01b6915cf67ad25ac8d7aff4ab8977b4320d06b
Eric <wunderkammer123@gmail.com>
2018-03-27 19:57:41 -0400

    Added GameResult class with DateTime field.

---
+++ Stats/GameResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stats
+{
+    public class GameResult
+    {
+        public DateTime GameDate { get; set; }
+
+    }
+}
