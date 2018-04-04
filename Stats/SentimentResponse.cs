b69869c523b6f0939d4ff62437968b8e2b946410
Eric <wunderkammer123@gmail.com>
2018-04-04 09:25:43 -0400

    Created SentimentResponse.cs

---
+++ Stats/SentimentResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stats
+{
+
+    public class SentimentResponse
+    {
+        [JsonProperty(PropertyName = "documents")]
+        public List<Sentiment> Sentiments { get; set; }
+        [JsonProperty(PropertyName = "errors")]
+        public List<object> Errors { get; set; }
+    }
+
+    public class Sentiment
+    {
+        [JsonProperty(PropertyName = "score")]
+        public float Score{ get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+
+}
