c55f07ae6f559ae39c0c3a57a5fac150d14daea4
Eric <wunderkammer123@gmail.com>
2018-04-04 08:26:20 -0400

    Created SentimentRequest.cs

---
+++ Stats/SentimentRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Stats
+{
+
+    public class SentimentRequest
+    {
+        [JsonProperty(PropertyName = "document")]
+        public List<Document> Documents { get; set; }
+    }
+
+    public class Document
+    {
+        [JsonProperty(PropertyName = "language")]
+        public string Language { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+    }
+
+}
